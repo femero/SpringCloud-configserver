82399b5696ab2222d0bc0c20d310bb56b249b068
federico1978 <femero1978@hotmail.com>
2024-11-20 19:02:22 -0500

    Se agrega el proyecto

---
+++ src/test/java/com/bootcamp/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
